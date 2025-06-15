5350c7b0c8648ed3cee31cbc3b5ba163c0d12098
Miala <65893041+Miala-python@users.noreply.github.com>
2025-06-15 13:46:41 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,8 +21,8 @@ let distance = 0
 let Volume = 0
 Volume = 50
 let DIST_MIN = 3
-let DIST_MAX = 100
-let FREQ_MIN = 100
+let DIST_MAX = 150
+let FREQ_MIN = 50
 let FREQ_MAX = 2000
 let LastFreq = 440
 let Freq = 440
@@ -33,20 +33,13 @@ basic.forever(function () {
     distTemp = 0
     nbTemp = 0
     if (DIST_MIN < distance && distance < DIST_MAX) {
-        Freq = FREQ_MIN + (FREQ_MAX - FREQ_MIN) * (DIST_MAX - distance) / (DIST_MAX - DIST_MIN)
-        // music.ringTone(Freq)
-        music.play(music.createSoundExpression(
-        WaveShape.Sine,
-        LastFreq,
-        Freq,
-        Volume,
-        Volume,
-        500,
-        SoundExpressionEffect.None,
-        InterpolationCurve.Linear
-        ), music.PlaybackMode.UntilDone)
+        Freq = FREQ_MIN + (distance - DIST_MIN) / DIST_MAX * (FREQ_MAX - FREQ_MIN)
+        music.ringTone(Freq)
         LastFreq = Freq
+    } else {
+        music.stopAllSounds()
     }
+    basic.pause(10)
 })
 basic.forever(function () {
     distTemp += sonar.ping(
